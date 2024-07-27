0403797f7b398d5022884e927b19f9501e155658
Gayathri-Gogireddy <143862109+Gayathri-Gogireddy@users.noreply.github.com>
2024-07-27 17:33:42 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "github" {
+  token        = var.github_token
+  organization = var.github_organization
+}
+
+resource "github_repository" "repos" {
+  for_each    = { for app in var.applications : app.name => app }
+  name        = each.value.name
+  description = each.value.description
+  visibility  = "private"
+  auto_init   = true
+}
